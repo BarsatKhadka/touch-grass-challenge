473c00e61bd5d139a969240b5cdf323796fabfb7
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-12 16:02:12 -0500

    add: logo rounded

--- frontend-vue/src/components/Home/Home.vue
+++ frontend-vue/src/components/Home/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 p-6">
     <div class="mb-8">
-      <img src="/logo.png" alt="Touch Grass Challenge Logo" class="w-40 h-auto" />
-    </div>
+  <img src="/logo.png" alt="Touch Grass Challenge Logo" class="w-40 h-40 rounded-full" />
+  </div>
+
     <div class="text-center">
       <h1 class="text-4xl font-bold mb-4">Welcome to the Touch Grass Challenge</h1>
       <p class="text-lg mb-6 max-w-xl mx-auto">
