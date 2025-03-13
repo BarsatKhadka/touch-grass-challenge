27acc7b71768a4d673aca3637768f1da684cddaf
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-13 11:11:48 -0500

    add: post,put,delete define in ChallengeService.js

--- frontend-vue/src/services/ChallengeService.js
+++ frontend-vue/src/services/ChallengeService.js
@@ -6,6 +6,16 @@ class ChallengeService {
     getChallenges() {
         return axios.get(CHALLENGES_API_BASE_URL);
     }
+    createChallenge(challenge) {
+        return axios.post(CHALLENGES_API_BASE_URL, challenge);
+    }
+    updateChallenge(id, challenge) {
+        return axios.put(`${CHALLENGES_API_BASE_URL}/${id}`, challenge);
+    }
+    deleteChallenge(id) {
+        return axios.delete(`${CHALLENGES_API_BASE_URL}/${id}`);
+    }
+
 }
 
 export default new ChallengeService();
