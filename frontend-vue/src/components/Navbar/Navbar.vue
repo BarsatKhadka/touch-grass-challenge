aa3ebf5ed5e98329df13b1d024ea80e6e36f2325
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-13 18:41:25 -0500

    add: navbar mainmenu for mobile

--- frontend-vue/src/components/Navbar/Navbar.vue
+++ frontend-vue/src/components/Navbar/Navbar.vue
@@ -28,6 +28,20 @@
                     </button>
                 </div>
             </div>
+
+            <!-- Mobile Logic -->
+            <div class="flex items-center sm:hidden ml-4">
+            <button @click="toggleMobileMenu" class="inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+                <span class="sr-only">Open main menu</span>
+                <svg v-if="!mobileMenuOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            </div>
+
     
 
 
