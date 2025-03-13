7036033dfbcdf558c5ec5096307520f36c6551e1
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-13 11:17:39 -0500

    add: add a challenge(post) works

--- frontend-vue/src/components/Home/AfterLogin/HomeAfterLogin.vue
+++ frontend-vue/src/components/Home/AfterLogin/HomeAfterLogin.vue
@@ -4,10 +4,17 @@
         <ul>
             <li v-for="challenge in challenges" :key="challenge.id">
                 {{ challenge.challengeName }}
+                <button @click="editChallenge(challenge)">Edit</button>
+                <button @click="deleteChallenge(challenge.id)">Delete</button>
             </li>
         </ul>
-    </div>
 
+        <h2>{{ isEditing ? 'Edit Challenge' : 'Add New Challenge' }}</h2>
+        <form @submit.prevent="isEditing ? updateChallenge() : addChallenge()">
+            <input v-model="challengeForm.challengeName" placeholder="Challenge Name" required />
+            <button type="submit">{{ isEditing ? 'Update' : 'Add' }}</button>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -17,13 +24,48 @@ export default {
     name: 'HomeAfterLogin',
     data() {
         return {
-            challenges: []
+            challenges: [],
+            challengeForm: {
+                id: null,
+                challengeName: ''
+            },
+            isEditing: false
         }
     },
     mounted() {
-        ChallengeService.getChallenges().then((response) => {
-            this.challenges = response.data;
-        });
+        this.fetchChallenges();
+    },
+    methods: {
+        fetchChallenges() {
+            ChallengeService.getChallenges().then((response) => {
+                this.challenges = response.data;
+            });
+        },
+        addChallenge() {
+            ChallengeService.createChallenge(this.challengeForm).then(() => {
+                this.fetchChallenges();
+                this.resetForm();
+            });
+        },
+        editChallenge(challenge) {
+            this.challengeForm = { ...challenge };
+            this.isEditing = true;
+        },
+        updateChallenge() {
+            ChallengeService.updateChallenge(this.challengeForm.id, this.challengeForm).then(() => {
+                this.fetchChallenges();
+                this.resetForm();
+            });
+        },
+        deleteChallenge(id) {
+            ChallengeService.deleteChallenge(id).then(() => {
+                this.fetchChallenges();
+            });
+        },
+        resetForm() {
+            this.challengeForm = { id: null, challengeName: '' };
+            this.isEditing = false;
+        }
     }
 }
 </script>
