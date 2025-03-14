f0489bba5f28797880dfdadc2b45c18bae4e0c0c
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-14 10:45:59 -0500

    add: small info box

--- frontend-vue/src/components/Home/AfterLogin/HomeAfterLogin.vue
+++ frontend-vue/src/components/Home/AfterLogin/HomeAfterLogin.vue
@@ -98,6 +98,18 @@
           Create your first challenge
         </button>
       </div>
+      <div class="mt-6 bg-blue-50 p-4 rounded-xl border border-blue-100">
+        <div class="flex items-center">
+          <div class="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center mr-3">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+            </svg>
+          </div>
+          <p class="text-sm text-blue-800">
+            Create meaningful challenges that connect you with nature.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
