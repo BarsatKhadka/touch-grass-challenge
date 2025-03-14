2f311a1c256866854552a09ae8ab5ba3cd0a16e8
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-14 13:31:08 -0500

    add: TheUser entity

---
+++ touch-grass-backend/src/main/java/com/barsat/touch_grass/Entity/TheUser.java
@@ -0,0 +1,52 @@
+package com.barsat.touch_grass.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@Data
+public class TheUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer masterId;
+
+    @Column(nullable = false , unique = true)  //username can't be null and should be unique
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    @Column(length = 10000)
+    private String avatarUrl;
+
+    private String bio ;
+
+    private boolean isEnabled;
+
+    private boolean emailVerified;
+
+
+    public TheUser(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public TheUser() {
+
+    }
+
+
+    //keep adding more fields if needed.
+}
