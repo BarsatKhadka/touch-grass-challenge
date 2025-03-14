53b15c0b3642f9189f46d86b1940ac59766188df
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-14 11:04:02 -0500

    add: Challenge status badges

--- frontend-vue/src/components/Home/AfterLogin/HomeAfterLogin.vue
+++ frontend-vue/src/components/Home/AfterLogin/HomeAfterLogin.vue
@@ -40,17 +40,22 @@
                       </div>
                     </div>
                   </div>
-                  <div class="flex space-x-2">
-                    <button @click="editChallenge(challenge.challengesId)" class="text-blue-500 hover:text-blue-700 p-2 rounded-full hover:bg-blue-50 transition-colors">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                      </svg>
-                    </button>
-                    <button @click="deleteChallenge(challenge.challengesId)" class="text-red-500 hover:text-red-700 p-2 rounded-full hover:bg-red-50 transition-colors">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                      </svg>
-                    </button>
+                  <div class="flex items-center">
+                    <span :class="getStatusBadgeClass(challenge.status)" class="px-2 py-1 rounded-full text-xs font-medium mr-3">
+                      {{ formatStatus(challenge.status) }}
+                    </span>
+                    <div class="flex space-x-2">
+                      <button @click="editChallenge(challenge.challengesId)" class="text-blue-500 hover:text-blue-700 p-2 rounded-full hover:bg-blue-50 transition-colors">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>
+                      </button>
+                      <button @click="deleteChallenge(challenge.challengesId)" class="text-red-500 hover:text-red-700 p-2 rounded-full hover:bg-red-50 transition-colors">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                          <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </li>
@@ -217,6 +222,29 @@ export default {
                 day: 'numeric',
                 year: 'numeric'
             });
+        },
+        formatStatus(status) {
+            if (!status) return 'Not Started';
+            return status.replace('_', ' ').toLowerCase()
+                .split(' ')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(' ');
+        },
+        getStatusBadgeClass(status) {
+            if (!status) return 'bg-gray-100 text-gray-800';
+            
+            switch(status) {
+                case 'NOT_STARTED':
+                    return 'bg-gray-100 text-gray-800';
+                case 'IN_PROGRESS':
+                    return 'bg-blue-100 text-blue-800';
+                case 'COMPLETED':
+                    return 'bg-green-100 text-green-800';
+                case 'CANCELLED':
+                    return 'bg-red-100 text-red-800';
+                default:
+                    return 'bg-gray-100 text-gray-800';
+            }
         }
     }
 }
