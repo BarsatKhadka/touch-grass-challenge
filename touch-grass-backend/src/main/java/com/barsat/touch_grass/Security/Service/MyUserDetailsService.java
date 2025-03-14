0074004e14946d7bf21deb5dff494a3a0a26dc5e
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-14 13:40:09 -0500

    add: MyUserDetailsService to link our custom user model with spring securtiy

--- touch-grass-backend/src/main/java/com/barsat/touch_grass/Security/Service/MyUserDetailsService.java
+++ touch-grass-backend/src/main/java/com/barsat/touch_grass/Security/Service/MyUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.barsat.touch_grass.Security.Service;
 
-public class MyUserDetailsService {
+import com.barsat.touch_grass.Entity.TheUser;
+import com.barsat.touch_grass.Entity.TheUserSecurityDetails;
+import com.barsat.touch_grass.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //look if user is in database
+        TheUser user = userRepo.findByUsername(username);
+
+        if (user == null) {
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("User with " + username + " not found");
+        }
+
+        //if the user is found , then pass that user to TheUserSecurityDetails to create UserDetailsType to return.
+        return new TheUserSecurityDetails(user);
+    }
 }
