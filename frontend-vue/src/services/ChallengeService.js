9e7053be5a327900145b6346783e40b7b5ee025c
BarsatKhadka <khadkabarsat598@gmail.com>
2025-03-14 21:06:50 -0500

    add: authorization header in every request

--- frontend-vue/src/services/ChallengeService.js
+++ frontend-vue/src/services/ChallengeService.js
@@ -4,16 +4,32 @@ const CHALLENGES_API_BASE_URL = 'http://localhost:8080/api/challenges';
 
 class ChallengeService {
     getChallenges() {
-        return axios.get(CHALLENGES_API_BASE_URL);
+        return axios.get(CHALLENGES_API_BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt')}`
+            }
+        });
     }
     createChallenge(challenge) {
-        return axios.post(CHALLENGES_API_BASE_URL, challenge);
+        return axios.post(CHALLENGES_API_BASE_URL, challenge, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt')}`
+            }
+        });
     }
     updateChallenge(id, challenge) {
-        return axios.put(`${CHALLENGES_API_BASE_URL}/${id}`, challenge);
+        return axios.put(`${CHALLENGES_API_BASE_URL}/${id}`, challenge, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt')}`
+            }
+        });
     }
     deleteChallenge(id) {
-        return axios.delete(`${CHALLENGES_API_BASE_URL}/${id}`);
+        return axios.delete(`${CHALLENGES_API_BASE_URL}/${id}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt')}`
+            }
+        });
     }
 
 }
